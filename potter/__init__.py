5ce0aa566753020c7d485383e72ee8718a20f79c
Chris Jessup <chris.jessup@hellofresh.com.au>
2022-08-12 11:10:22 +1000

    Small Fix

--- potter/__init__.py
+++ potter/__init__.py
@@ -5,19 +5,14 @@ def _magic():
     import time, random
 
     originalstring = '          Harry Potter says "imports are magic!"' 
-    string = list(originalstring)
 
-    d = {}
-    for i, ch in enumerate(string):
-        d[i] = ch
+    first, rest = [], list(originalstring)
 
-    first, rest = [], string
-
-    for i in range(len(string)):
+    for i in range(len(originalstring)):
         print(''.join(first + rest), end='\r')
         time.sleep(0.1)
 
-        random.shuffle(string)
+        random.shuffle(rest)
         first.append(originalstring[i])
         rest.pop(rest.index(originalstring[i]))
      
